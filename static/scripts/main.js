5becd4a0b241e967c6e29bd6643e42def13253c1
João <docafavarato@gmail.com>
2023-05-24 14:17:57 -0300

    first

---
+++ static/scripts/main.js
@@ -0,0 +1,41 @@
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('nav a');
+const moreButton = document.querySelector('#moreAbout')
+
+function changeSection(e) {
+  e.preventDefault();
+
+  const clickedLink = e.target;
+  navLinks.forEach(link => {
+    link.classList.remove('current');
+  });
+
+  clickedLink.classList.add('current');
+
+  sections.forEach(section => {
+    section.classList.remove('active');
+  });
+
+  const target = clickedLink.getAttribute('href');
+  document.querySelector(target).classList.add('active');
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('click', changeSection);
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', changeSection)
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    link.classList.add('current');
+    console.log(link.classList);
+  });
+});
+
+moreButton.addEventListener('click', changeSection);
+moreButton.addEventListener('click', function(e) {
+  document.querySelector('#location').classList.add('current');
+})
